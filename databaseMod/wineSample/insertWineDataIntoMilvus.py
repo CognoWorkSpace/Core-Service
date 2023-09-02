121daa579db2731dd014ebf63ec1033d7c8c20c0
XinkaiWang515 <xinkaiwang515@gmail.com>
2023-09-02 09:59:44 -0700

    feat[18] insert processed dataframe into milvus

--- databaseMod/wineSample/insertWineDataIntoMilvus.py
+++ databaseMod/wineSample/insertWineDataIntoMilvus.py
@@ -8,12 +8,13 @@ pd.set_option('display.max_columns', None)
 mvs_db = MilvusDB()
 mvs_db.db_name = 'wine'
 
-df = mvs_db.read_df_with_vector(csv_file_path='wineData/wine_data_w_embed.csv',
+df = mvs_db.read_df_with_vector(csv_file_path='wineData/wine_data_from_aws.csv',
                                 embedded_col='wine_info_embed',
                                 index_col=None)
 df.drop(columns=['combined'], inplace=True)
+df['points'] = df['points'].astype('int16')
 df['price'] = df['price'].astype('float32')
-print(df.head())
+# print(df.head())
 
 new_collection_name = "wine_data"
 
@@ -23,4 +24,6 @@ mvs_db.collection = mvs_db.create_collection_by_df(
     schema_description="wine parameters data with combination embedding",
     pk_field_name=None,
 )
+
+mvs_db.insert_df_into_collection(df)
 
